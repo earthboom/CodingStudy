efc10ff5197759836968ec7410a1436f82aa9dc9
earthboom <earthboom@naver.com>
2019-05-07 21:03:14 +0900

    00:36 Texture KKH

--- KKH/Direct3D_Project/Direct3D_Project/Const.h
+++ KKH/Direct3D_Project/Direct3D_Project/Const.h
@@ -20,6 +20,3 @@ const float SCREEN_NEAR = 0.1f;
 const float PI = 3.1415926535897932f;
 
 const int NumFrameResources = 3;
-
-
-
--- KKH/Direct3D_Project/Direct3D_Project/Function.h
+++ KKH/Direct3D_Project/Direct3D_Project/Function.h
@@ -3,6 +3,7 @@
 #include <comdef.h>
 #include <fstream>
 #include <string>
+#include <array>
 #include "Const.h"
 
 class DxException
@@ -73,3 +74,54 @@ inline std::wstring AnsiToWString(const std::string& str)
 
 
 inline const float AspectRatio(void) { return static_cast<float>(WINSIZE_X) / WINSIZE_Y; }
+
+inline std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> GetStaticSamplers(void)
+{
+	const CD3DX12_STATIC_SAMPLER_DESC pointWrap(
+		0,									//shaderRegister
+		D3D12_FILTER_MIN_MAG_MIP_POINT,		//filter
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,	//addressU
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,	//addressV
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP);	//addressW
+
+	const CD3DX12_STATIC_SAMPLER_DESC pointClamp(
+		1,
+		D3D12_FILTER_MIN_MAG_MIP_POINT,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP);
+
+	const CD3DX12_STATIC_SAMPLER_DESC linearWrap(
+		2,
+		D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP);
+
+	const CD3DX12_STATIC_SAMPLER_DESC linearClamp(
+		3,
+		D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP);
+
+	const CD3DX12_STATIC_SAMPLER_DESC anisotropicWrap(
+		4,
+		D3D12_FILTER_ANISOTROPIC,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		0.0f,								//mipLODBias
+		8);									//maxAnisotropy
+
+	const CD3DX12_STATIC_SAMPLER_DESC anisotropicClamp(
+		5,
+		D3D12_FILTER_ANISOTROPIC,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		D3D12_TEXTURE_ADDRESS_MODE_CLAMP,
+		0.0f,
+		8);
+
+	return { pointWrap, pointClamp, linearWrap, linearClamp, anisotropicWrap ,anisotropicClamp };
+}
