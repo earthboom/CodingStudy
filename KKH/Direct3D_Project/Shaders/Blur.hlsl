4d11ff3487b51662575d44cd6dd911f99d90939f
earthboom <33519279+earthboom@users.noreply.github.com>
2019-06-19 00:12:44 +0900

    00:69 Compute Shader (KKH)

--- KKH/Direct3D_Project/Shaders/Blur.hlsl
+++ KKH/Direct3D_Project/Shaders/Blur.hlsl
@@ -60,5 +60,31 @@ void HorzBlurCS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : S
 [numthreads(1, N, 1)]
 void VertBlurCS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID)
 {
+	float weights[11] = { w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10 };
 
+	if (groupThreadID.y < gBlurRadius)
+	{
+		int y = max(dispatchThreadID.y - gBlurRadius, 0);
+		gCache[groupThreadID.y] = gInput[int2(dispatchThreadID.x, y)];
+	}
+
+	if (groupThreadID.y >= (N - gBlurRadius))
+	{
+		int y = min(dispatchThreadID.y + gBlurRadius, gInput.Length.y - 1);
+		gCache[groupThreadID.y + 2 * gBlurRadius] = gInput[int2(dispatchThreadID.x, y)];
+	}
+
+	gCache[groupThreadID.y + gBlurRadius] = gInput[min(dispatchThreadID.xy, gInput.Length.xy - 1)];
+
+	GroupMemoryBarrierWithGroupSync();
+
+	float4 blurColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+	for (int i = -gBlurRadius; i <= gBlurRadius; ++i)
+	{
+		int k = groupThreadID.y + gBlurRadius + i;
+		blurColor += weights[i + gBlurRadius] * gCache[k];
+	}
+
+	gOutput[dispatchThreadID.xy] = blurColor;
 }
