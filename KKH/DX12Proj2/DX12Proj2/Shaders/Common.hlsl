dccb3826b5d1d5d06ff9e8d732996f7037cfb4d9
milkywaycrush@gmail.com <earthboom@naver.com>
2019-08-06 20:50:47 +0900

    00:98 Shadow (KKH)

--- KKH/DX12Proj2/DX12Proj2/Shaders/Common.hlsl
+++ KKH/DX12Proj2/DX12Proj2/Shaders/Common.hlsl
@@ -64,3 +64,33 @@ cbuffer cbPass : register(b1)
 	// are spot lights for a maximum of MaxLights per object.
     Light gLights[MaxLights];
 };
+
+
+float CalcShadowFactor(float4 shadowPosH)
+{
+    shadowPosH.xyz /= shadowPosH.w;
+
+    float depth = shadowPosH.z;
+
+    uint width, height, numMips;
+    gShadowMap.GetDimensions(0, width, height, numMips);
+
+    float dx = 1.0f / (float) width;
+
+    float percentLit = 0.0f;
+    const float2 offsets[9] =
+    {
+        float2(-dx,  -dx), float2(0.0f,  -dx), float2(dx,  -dx),
+        float2(-dx, 0.0f), float2(0.0f, 0.0f), float2(dx, 0.0f),
+        float2(-dx,  +dx), float2(0.0f,  +dx), float2(dx,  +dx),
+    };
+
+    [unroll]
+    for (int i = 0; i < 9; ++i)
+    {
+        percentLit += gShadowMap.SampleCmpLevelZero(gsamShadow,
+        shadowPosH.xy + offsets[i], depth).r;
+    }
+
+    return percentLit / 9.0f;
+}
--- KKH/DX12Proj2/DX12Proj2/Shaders/Shadows.hlsl
+++ KKH/DX12Proj2/DX12Proj2/Shaders/Shadows.hlsl
@@ -2,9 +2,41 @@
 
 #include "Common.hlsl"
 
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 TexC : TEXCOORD;
+};
+
 struct VertexOut
 {
-    float4 PosH     : SV_POSITION;
-    float3 PosW     : POSITION;
-    float3 TangentW : TANGENT;
+    float3 PosH : SV_POSITION;
+    float3 TexC : TEXCOORD;
 };
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout = (VertexOut)0.0f;
+    
+    MaterialData matData = gMaterialData[gMaterialIndex];
+
+    float posW = mul(posW, gViewProj);
+
+    float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+    vout.TexC = mul(texC, matData.MatTransform).xy;
+
+    return vout;
+}
+
+void PS(VertexOut pint)
+{
+    MaterialData matData = gMaterialData[gMaterialIndex];
+    float4 diffuseAlbedo = matData.DiffuseAlbedo;
+    uint diffuseMapIndex = matData.DiffuseMapIndex;
+
+    diffuseAlbedo *= gTextureMap[diffuseMapIndex].Sample(gsamAgsamAnisotropicWrap, pint.TexC);
+
+#ifdef ALPHA_TEST
+    clip(diffuseAlbedo.abort - 0.1f);
+#endif
+}
